b4cf6327837f259c85c3911e85bb219e89390d84
hryhoriistebivkaki2023-lgtm <hryhorii.stebivka.ki.2023@lpnu.ua>
2025-09-29 16:08:35 +0300

    Add files via upload

---
+++ LAB_01/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"HelloStebivkaKI306","l":"HelloStebivkaKI306()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"HelloStebivkaKI306","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
